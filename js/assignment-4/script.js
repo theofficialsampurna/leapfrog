2b50b7816749e1d47b9f1c366dcbdeaaeb86c461
theofficialsampurna <theofficialsampurnashrestha@gmail.com>
2019-12-02 02:41:41 +0545

    additional codes for assignment 4

--- js/assignment-4/script.js
+++ js/assignment-4/script.js
@@ -2,8 +2,9 @@ var parentElement = document.getElementById('app');
 var road1 = new roadFighter(parentElement);
 road1.gameInit();
 
+
 function roadFighter(parentElement) {
-  var MAX_WIDTH = 440;
+  var MAX_WIDTH = 600;
   var MAX_HEIGHT = 480;
   var BG_HEIGHT = 960;
   var x = 0;
@@ -18,9 +19,15 @@ function roadFighter(parentElement) {
   var posLane3 = 260;
   var progress = 60;
   var obstacles = [];
-  var obstaclesNo = 10;
+  var obstaclesNo = 5;
   var speedObs = 3;
-
+  var mainCar = new Car();
+  var gameOver = document.createElement('div');
+  var scoreBoard = document.createElement('div');
+  var score = 0;
+  var scoreText;
+  this.request = 0;
+  var scoreBoard = document.createElement('div');
 
   this.gameInit = function () {
     this.createAll();
@@ -43,50 +50,66 @@ function roadFighter(parentElement) {
     gameWrapper.style.left = 0
     gameWrapper.style.background = "url('images/roadfightergrass.png')";
     gameWrapper.style.backgroundRepeat = 'no-repeat';
-
     parentElement.appendChild(gameWrapper);
 
-    var mainCar = new Car();
+    gameOver.style.background = "url('images/gameover.jpg')";
+    gameOver.style.backgroundRepeat = 'no-repeat';
+    // gameOver.style.backgroundSize = 'c';
+    gameOver.style.position = 'absolute';
+    gameOver.style.display = 'none';
+    gameOver.style.zIndex = '100';
+    gameOver.style.opacity = '0.9';
+    gameOver.style.width = 480 + 'px';
+    gameOver.style.height = MAX_HEIGHT + 'px';
+    parentElement.appendChild(gameOver);
+
+    scoreBoard.style.position = 'absolute';
+    scoreBoard.style.zIndex = '100';
+    scoreBoard.style.left = 470 + 'px';
+    scoreBoard.style.top = 50 + 'px';
+    scoreBoard.style.color = 'white';
+    parentElement.appendChild(scoreBoard);
+
+
     mainCar.init();
-    for (var i = 0; i < obstaclesNo; i++) {
-      generateObstacles(i);
-      // var obstacleOverlap = generateObstacles(i);
-      // if (obstacleOverlap == 'true')
-      //   i--;
+    for (var i = 0; i <= (obstaclesNo - 1); i++) {
+      generateObstacle(i);
     }
 
   }
 
-  function generateObstacles(i) {
-    var obstacle = new Obstacles();
+  function generateObstacle(index) {
+    var obstacle = new Obstacle();
     obstacle.init();
     var posX = parseInt(getRandomArbitrary(0, 3));
     var posY = parseInt(getRandomArbitrary(-1000, -100));
-    // var obstacleOverlap = checkPosition(posX, posY);
-    // if (obstacleOverlap == 'false') {
-      obstacle.setPosition(posX, posY);
-      obstacle.drawObstacle();
-      obstacles.splice(i, 0, obstacle);
-    // }
-    // return obstacleOverlap;
+    var overlapStatus = checkOverlap(posX, posY);
+    while (overlapStatus == 'true') {
+      posY = parseInt(getRandomArbitrary(-1000, -100));
+      overlapStatus = checkOverlap(posX, posY);
+    }
+
+    obstacle.setPosition(posX, posY);
+    obstacle.drawObstacle();
+    obstacles.splice(index, 0, obstacle);
+
   }
 
-  function checkPosition(posX, posY) {
-    if ((obstacles.length == 0) || (posX == 'undefined') || (posY == 'undefined')) {
+  function checkOverlap(posx, posy) {
+    posx = renderXvalue(posx);
+    if (obstacles.length == 0) {
       return 'false';
     }
-    else {
-      posX = renderXvalue(posX);
-      for (var i = 0; i < obstacles.length; i++) {
-        if (((posX >= (obstacles[i].x - 60)) && (posX <= (obstacles[i].x + 60))) && ((posY >= (obstacles[i].y - 120)) && (posY <= (obstacles[i].y + 120)))) {
-          return 'true';
-        }
-        else
-          var overlapStatus = 'false';
-      }
-    }
-    return overlapStatus;
+    for (var j = 0; j <= obstacles.length; j++) {
+      var a = Math.abs(posx - obstacles[j].x);
+      var b = Math.abs(posy - obstacles[j].y);
 
+      if (b <= 150) {
+        return 'true';
+      }
+      else
+        return 'false';
+    }
   }
 
   function renderXvalue(x) {
@@ -108,16 +131,18 @@ function roadFighter(parentElement) {
     }
   }
 
-  //gameloop start //
+  //----------------------------------------------gameloop start 
   function draw() {
     moveBg();
     moveObstacles();
+    checkCollision();
+    previewScore();
 
-    window.requestAnimationFrame(draw);
+    this.request = window.requestAnimationFrame(draw);
   }
+  this.request = window.requestAnimationFrame(draw);
 
-  window.requestAnimationFrame(draw);
-  //gameloop end //
+  //----------------------------------------------gameloop end 
 
   function moveBg() {
     y -= speed;
@@ -127,19 +152,35 @@ function roadFighter(parentElement) {
     gameWrapper.style.bottom = y + 'px';
   }
 
-  function moveObstacles() {
-    for (var i = 0; i < obstaclesNo; i++) {
-      obstacles[i].y += speedObs;
-      obstacles[i].drawObstacle();
+  function previewScore() {
+    scoreBoard.innerHTML = ('Score: ' + score);
+  }
 
-      if (obstacles[i].y > 500) {
-        obstacles.splice(i, 1);
-        generateObstacles(i);
+  function moveObstacles() {
+    for (var k = 0; k < obstacles.length; k++) {
+      obstacles[k].y += speedObs;
+      obstacles[k].drawObstacle();
+
+      if (obstacles[k].y > 500) {
+        obstacles[k].remove();
+        obstacles.splice(k, 1);
+        score += 1;
+        generateObstacle(k);
       }
-      console.log(obstacles);
+
     }
   }
 
+  function checkCollision() {
+    var collisionState = mainCar.checkCollision(obstacles);
+    if (collisionState == 'true') {
+      gameOver.style.display = 'block';
+      obstacles = [];
+      window.cancelAnimationFrame(this.request);
+    }
+  }
+
+  // --------------------------------------------main Car object
   function Car() {
     that = this;
     this.x = 200;
@@ -158,6 +199,7 @@ function roadFighter(parentElement) {
       this.element.style.position = 'absolute';
       this.element.style.backgroundImage = "url('images/main-car.png') ";
       this.element.style.backgroundPosition = 'center bottom';
+      this.element.style.zIndex = '50';
       this.element.style.top = this.y + 'px';
       this.element.style.left = this.x + 'px';
       parentElement.appendChild(this.element);
@@ -192,6 +234,16 @@ function roadFighter(parentElement) {
       }
     }
 
+    this.checkCollision = function (enemy) {
+      for (var i = 0; i < enemy.length; i++) {
+        if (this.x <= (enemy[i].x + enemy[i].width) && (this.x + this.width) >= enemy[i].x && this.y <= (enemy[i].y + enemy[i].height) && (this.y + this.height) >= enemy[i].y) {
+          console.log('collision!!!');
+          this.element.style.display = 'none';
+          return 'true';
+        }
+      }
+      return false;
+    }
     window.addEventListener("keydown", this.moveCar, false);
   }
 
@@ -199,7 +251,8 @@ function roadFighter(parentElement) {
     return Math.random() * (max - min) + min;
   }
 
-  function Obstacles() {
+  // -----------------------------------------Obstacles object
+  function Obstacle() {
     this.x = 3;
     this.y = 100;
     this.height = 60;
@@ -211,9 +264,10 @@ function roadFighter(parentElement) {
       this.elementObs = document.createElement('div');
       this.elementObs.style.height = this.height + 'px';
       this.elementObs.style.width = this.width + 'px';
-      this.elementObs.classList.add('obstacles');
+      this.elementObs.classList.add('obstacle');
       this.elementObs.style.position = 'absolute';
       this.elementObs.style.backgroundImage = "url('images/enemy-2.png') ";
+      this.elementObs.style.zIndex = '50';
       this.elementObs.style.backgroundPosition = 'center bottom';
       // this.setPosition(this.x, this.y);
       parentElement.appendChild(this.elementObs);
@@ -242,6 +296,10 @@ function roadFighter(parentElement) {
     this.drawObstacle = function () {
       this.elementObs.style.left = this.x + 'px';
       this.elementObs.style.top = this.y + 'px';
+    }
+
+    this.remove = function () {
+      parentElement.removeChild(this.elementObs);
     }
 
   }
